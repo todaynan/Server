16a8de14774a0bec0b3bdffc60fa24d004a5f508
RakunKo <huncher00@naver.com>
2024-07-15 21:11:36 +0900

    prefer 수정

--- src/main/java/umc/todaynan/domain/entity/User/UserPrefer/PreferCategory.java
+++ src/main/java/umc/todaynan/domain/entity/User/UserPrefer/PreferCategory.java
@@ -3,23 +3,16 @@ package umc.todaynan.domain.entity.User.UserPrefer;
 import jakarta.persistence.*;
 import lombok.*;
 import umc.todaynan.domain.common.DateBaseEntity;
-import umc.todaynan.domain.entity.User.User.User;
-
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class UserPrefer extends DateBaseEntity {
+public class PreferCategory  extends DateBaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private PreferCategory preferCategory;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(nullable = false, length = 15)
+    private String title;
 }
