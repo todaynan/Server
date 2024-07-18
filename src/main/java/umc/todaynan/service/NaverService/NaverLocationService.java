30ec23a94ad872175d91f167c30fa360f4c55793
RakunKo <huncher00@naver.com>
2024-07-18 18:51:10 +0900

    dev release

--- src/main/java/umc/todaynan/service/NaverService/NaverLocationService.java
+++ src/main/java/umc/todaynan/service/NaverService/NaverLocationService.java
@@ -38,6 +38,7 @@ public class NaverLocationService {
                     .queryParam("query", searchString)
                     .queryParam("display", 5)
                     .queryParam("start", 1)
+                    .queryParam("sort", "random")
                     .encode(Charset.forName("UTF-8"))
                     .build()
                     .toUri();
